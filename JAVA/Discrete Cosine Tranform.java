b20d3aba87db82d6750653b3c728632101d0df84
Ashish Kaktan <kaktan.ashish@gmail.com>
2017-03-27 15:21:22 +0530

    Update Discrete Cosine Tranform.java

--- JAVA/Discrete Cosine Tranform.java
+++ JAVA/Discrete Cosine Tranform.java
@@ -3,39 +3,30 @@ import java.math.*;
 
 class DCT{
 	
+	static double[] c;
+	static void init(){
+		c=new double[32];
+		for(int i=1;i<32;i++){
+			c[i]=1.0;
+		}
+		c[0]=1/Math.sqrt(2.0);
+	}
+	
 	static int [][] img_dct(int [][]in){
 		
-		double gray=0,dct=0,sum=0,au=0,av=0,cosi=0,cosj=0,coen=0,coem=0,pi=3;
 		int [][]result = new int[32][32];
-	
+		init();
 		for(int u=0;u<32;u++){
 			for(int v=0;v<32;v++){
-					
-				sum=0.0;
+				double sum=0.0;
 								
 				for(int i=0;i<32;i++){
 					for(int j=0;j<32;j++){
-						
-					if(i==0)
-						au=1/Math.sqrt(2);
-					else
-						au=1;
-					if(j==0)
-						av=1/Math.sqrt(2);
-					else
-						av=(1);			
-							
-					gray = in[i][j];	
-					cosi = Math.cos((pi*u*(2*i+1))/64);
-					cosj = Math.cos((pi*v*(2*j+1))/64);
-					
-					dct=gray*cosi*cosj*au*av;
-					sum+=dct;			
+						sum+=Math.cos(((2*i+1)/(2.0*32))*u*Math.PI)*Math.cos(((2*i+1)/(2.0/32))*v*Math.PI)*in[i][j];		
 					}
 				}
-				coen=Math.sqrt(0.015625);
-				coem=Math.sqrt(0.03125);
-				result[u][v]=(int)(sum*coen*coem);
+				sum=sum*((c[u]*c[v])/4.0);
+				result[u][v]=sum;
 			}
 		}
 		return result;
